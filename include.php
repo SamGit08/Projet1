aa5566b7d2bec0227c16ce3474bb689c28c3065a
sambou <samboudiallo@ymail.com>
2017-03-28 22:02:09 +0200

    first commit

---
+++ include.php
@@ -0,0 +1,3 @@
+<object type="application/x-shockwave-flash" data="https://www.youtube.com/embed/uSXoUDIeuck">
+      <param name="movie" value="https://www.youtube.com/embed/uSXoUDIeuck" />
+</object>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+      <link rel="stylesheet" href="style.css"/>
+    <title>Accueil</title>
+  </head>
+  <body>
+    <header>
+      <p class="big">Accueil</p>
+      <img class="logo" src="file:///home/sambou/projet1/cropped-lowhi.jpg"/><!--
+-->
+  <h1>Présentation</h1>
+<?php
+include 'include.php';
+ ?>
+  </header>
+    <main>
+    <aside>
+      <nav>
+        <ul>
+          <li><a href="file:///home/sambou/projet1/Formation.html">Formations</a></li>
+          <li><a href="file:///home/sambou/projet1/Experiences.html">Experiences</a></li>
+          <li><a href="file:///home/sambou/projet1/Compétences.html">Compétences</a></li>
+          <li><a href="file:///home/sambou/projet1/Loisirs.html">Loisirs</a></li>
+        </ul>
+      </nav>
+    </aside><!--
+    --><section class="container">
+        <article >
+            <p class="texte">Bonjour !<br><br>
+                Je m'appelle Sambou j'ai 28 ans et j'habite Paris.
+                J'aime la composition, les films d'arts martiaux, de comics
+                les sorties entre amis et passer du bon temps.</p>
+        </article>
+    </section><!--
+    --><section class="droite">
+        <article class="container">
+            <p class="texte"></p>
+        </article>
+    </section>
+  </main>
+  <footer>
+    <ul>
+        <li class="foot"><a href="file:///home/sambou/projet1/Formation.html">Suivant</a></li>
+        <li class="foot"><a href="file:///home/sambou/projet1/Contacts.html">Contacts</a></li>
+    </ul>
+  </footer>
+  </body>
+</html>
